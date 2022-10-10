4d8c0ab6628039954ec07a4cae84fb9988b412e0
pabloisla <114646785+pabloisla@users.noreply.github.com>
2022-10-10 10:16:04 +0200

    Add files via upload

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,44 +1,16 @@
-package boletin1;
-
-import java.util.Scanner;
+package boletin2;
 
 public class Ejercicio1 {
-
-	private static Scanner teclado = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		
-		float porcentaje;
-		
-		System.out.println("Introduce tu edad");
-		int edad = Integer.parseInt(teclado.nextLine());
-		 char estado;
-		 do {
-			 System.out.println("Introduce tu estado civil (C, S, V, D");
-			 estado = teclado.nextLine().toUpperCase().charAt(0)
-		while(estado != 'C' && estado != 'S' && estado != 'V' && estado != 'D')0
-		
-		if (edad > 50) {
-		 porcentaje = 8.5f;
-		}
-		else if (edad < 35) {
-			switch (estado) {
-			case 'D':
-			case 'S':
-				porcentaje =12f;
-				break;
-			default:
-			case 'V':
-			case 'C':
-				porcentaje = 11.3f;
-				break;
+		for(int i = 1 ; i <=100; i++)
+		{
+			if (i % 7 == 0) {
+				System.out.printf("%d Es multiplo de 7 \n" , i);
+			}
+			if (i % 13 == 0) {
+				
 			}
 		}
-			else {
-				porcentaje = 10.5f;
-			}
-			
-			 System.out.printtf("El porcentaje a aplicar es %f", porcentaje);
-
-}
+	}
 }
