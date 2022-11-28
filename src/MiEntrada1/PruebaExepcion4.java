9af3d3d5fc981d52babca139334e73c07a8377db
pabloisla <114646785+pabloisla@users.noreply.github.com>
2022-11-28 09:54:05 +0100

    Add files via upload

---
+++ src/MiEntrada1/PruebaExepcion4.java
@@ -0,0 +1,15 @@
+package MiEntrada1;
+
+public class PruebaExepcion4 {
+    public static void main(String[] args) {
+        String mensaje1 = "Introduce un numero";
+        String mensaje2 = "Introduce un numero positivo";
+        String mensaje3 = "Introduce un numero entre 10 y 20";
+
+        System.out.println(MiEntradaSalida.solicitarEntero(mensaje1));
+        System.out.println(MiEntradaSalida.solicitarEnteroPositivo(mensaje2));
+        System.out.println(MiEntradaSalida.solicitarEnteroEnRango(mensaje3, 10, 20));
+
+    }
+
+}
